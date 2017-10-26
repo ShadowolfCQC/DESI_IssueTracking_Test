1969a4ac425c28bc1f951a496098443304947246
ShadowolfCQC <33138270+ShadowolfCQC@users.noreply.github.com>
2017-10-26 19:23:34 -0400

    Create CCD_Detector.py

---
+++ CCD_Detector.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python
+
+import numpy
+from astro.table import Table
+from matplotlib import rcParams
+from matplotlib import pyplot
+from photutils.datasets import make_gaussian_sources
+from photutils.datasets import make_noise_image
+import random
+
+#Cr
